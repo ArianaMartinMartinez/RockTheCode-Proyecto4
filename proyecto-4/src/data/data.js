81801d4ca3582730124428b7dddbc704d12abdf0
Ariana Martin <arianamartinmartinez@gmail.com>
2024-07-24 22:11:22 +0200

    Objeto con datos rellenos hecho

---
+++ proyecto-4/src/data/data.js
@@ -0,0 +1,121 @@
+export const data = {
+    fullName: 'Ariana Martín Martínez',
+    photo: '../../image/Ariana.jpg',
+    job: 'Full-stack web developer',
+    aboutMe: `
+    Soy una desarrolladora web junior que busca formarse continuamente para poder conseguir su primera experiencia
+    laboral en el mundo de la informática. Me considero una persona resolutiva y responsable con ganas de trabajar
+    con pasión.
+
+    Nací, me crié y vivo en Málaga por lo que soy una apasionada de pasar el día en la playa, dando un paseo mientras
+    escucho el sonido de las olas del mar. En mi tiempo libre me gusta salir con mis amigos a hacer cualquier plan,
+    ver series o películas y leer lo que puedo; aunque mi gran pasión es el crochet.
+
+    Hace unos 8 años que mi madre me enseñó y desde entonces me ha encantado este hobby (aunque muchas veces sea considerado
+    para abuelas). Me encanta poder hacerme mi propia ropa y accesorios; además, desde que era pequeña me han encatado
+    las manualidades por lo que este es el hobby perfecto para mí.
+
+    En resumen, soy una persona a la que le encantan las manualidades y la tecnología, por lo que estoy ansiosa por 
+    seguir aprendiendo día a día.
+    `,
+    skills: [
+        {
+            name: 'HTML5',
+            logo: 'https://kinsta.com/wp-content/uploads/2021/03/HTML-5-Badge-Logo.png',
+        },
+        {
+            name: 'CSS3',
+            logo: 'https://static-00.iconduck.com/assets.00/file-type-css-icon-902x1024-dqy5inwy.png',
+        },
+        {
+            name: 'JavaScript',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+        },
+        {
+            name: 'Angular',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png',
+        },
+        {
+            name: 'MySQL',
+            logo: 'https://pngimg.com/d/mysql_PNG9.png',
+        },
+        {
+            name: 'Java',
+            logo: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/181_Java_logo_logos-512.png',
+        },
+        {
+            name: 'Spring Boot',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Spring_Boot.svg/640px-Spring_Boot.svg.png',
+        },
+        {
+            name: 'Git',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png',
+        },
+    ],
+    education: [
+        {
+            degree: 'Grado Superior en Desarrollo de Aplicaciones Web',
+            school: 'IES Politécnico Jesús Marín',
+            startYear: 2021,
+            finishYear: 2023,
+        },
+        {
+            degree: 'Bootcamp Rock{TheCode} Desarrollo Web Full-stack',
+            school: 'ThePower Education',
+            startYear: 2024,
+            finishYear: 2025,
+        },
+    ],
+    experience: [
+        {
+            job: 'Prácticas Desarrolladora web junior',
+            company: 'DEKRA Testing and Certification, S.A.U.',
+            year: 2023,
+        },
+        {
+            job: 'Auxiliar Administrativa',
+            company: 'OPPLUS',
+            year: 2024,
+        },
+    ],
+    proyects: [
+        {
+            name: 'PcComponentes',
+            photo: '../../image/proyecto1.png',
+            description: 'Proyecto realizado en HTML y CSS básico para recrear la web de PcComponentes',
+            url: 'https://github.com/ArianaMartinMartinez/RockTheCode-Proyecto1',
+        },
+        {
+            name: "Ariana's Jewelry",
+            photo: '../../image/proyecto2.png',
+            description: 'Proyecto que simula una tienda de joyas en la cual podemos filtrar tanto por marca como por precio',
+            url: 'https://github.com/ArianaMartinMartinez/RockTheCode-Proyecto2',
+        },
+        {
+            name: "Ariana's Gallery",
+            photo: '../../image/proyecto3.png',
+            description: 'Proyecto realizado con la ayuda de la API Unsplash que muestra fotografías recreando la web de Pinterest',
+            url: 'https://github.com/ArianaMartinMartinez/RockTheCode-Proyecto3',
+        },
+        {
+            name: 'Portfolio',
+            photo: '',
+            description: 'Proyecto de mi portfolio para presentarme y mostrar mis conocimientos y experiencias',
+            url: 'https://github.com/ArianaMartinMartinez/RockTheCode-Proyecto4',
+        },
+    ],
+    contact: [
+        {
+            name: 'Mail',
+            url: 'arianamartinmartinez@gmail.com',
+        },
+        {
+            name: 'GitHub',
+            url: 'https://github.com/ArianaMartinMartinez',
+        },
+        {
+            name: 'Linkedin',
+            url: 'https://www.linkedin.com/in/ariana-martin-martinez',
+        },
+    ],
+};
